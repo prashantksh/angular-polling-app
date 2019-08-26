3112cbe114e6abe983bfcf138182daacd2bcfffb
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-08-26 13:43:13 +0530

    init for starter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+class Car {
+  name = 'BMW';
+  owner = 'ABCD';
+  Speed = 120;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'polling-app';
+  title = 'Hi there!';
+  today = new Date();
+  pi = 3.14159265359;
+  rate = 0.67;
+  car = new Car();
 }
